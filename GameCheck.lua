e6e84e45fbf2867395dc19756221706061ca9ff7
Anjelo <78515473+TheCoder005@users.noreply.github.com>
2021-05-01 18:36:06 -0400

    Update GameCheck.lua

--- GameCheck.lua
+++ GameCheck.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 286090429 then
-    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheCoder005/AnjeloScripts-Hub/TheCoder005-Game-Scripts/Arsenal.lua"))()
 end
 
 --elseif game.PlaceId == 3956818381 then
