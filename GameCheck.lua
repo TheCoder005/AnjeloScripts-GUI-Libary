8652f2691dacc063f79652260ebc0e0390c80d4c
Anjelo <78515473+TheCoder005@users.noreply.github.com>
2021-05-01 18:35:47 -0400

    Create GameCheck.lua

---
+++ GameCheck.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 286090429 then
+    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+end
+
+--elseif game.PlaceId == 3956818381 then
+    
+--end
