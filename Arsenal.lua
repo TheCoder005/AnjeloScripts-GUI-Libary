5ff5a715f289f2bc82cb496dbf53e3db27127e9e
Anjelo <78515473+TheCoder005@users.noreply.github.com>
2021-05-02 19:51:14 -0400

    Update Arsenal.lua

--- Arsenal.lua
+++ Arsenal.lua
@@ -1,5 +1,4 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-    local Window = Library.CreateLib("AnjeloScripts ", Theme)
+
     
     --tabs
     local Main = Window:NewTab("Main")
