b22ce8d1c1c46da634bb88705bb68559f4aa71f9
Anjelo <78515473+TheCoder005@users.noreply.github.com>
2021-05-01 20:39:57 -0400

    Update GameCheck.lua

--- GameCheck.lua
+++ GameCheck.lua
@@ -3,7 +3,7 @@ if game.PlaceId == 286090429 then
     local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheCoder005/AnjeloScripts-Hub/TheCoder005-Game-Scripts/Arsenal.lua"))()
 --SL2
 elseif game.PlaceId == 301549746 then
-     local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheCoder005/AnjeloScripts-Hub/TheCoder005-Game-Scripts/CB.lua"))()
+     local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheCoder005/AnjeloScripts-Hub/TheCoder005-Game-Scripts/SL2.lua"))()
 --CB    
 elseif game.PlaceId == 301549746 then
      local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheCoder005/AnjeloScripts-Hub/TheCoder005-Game-Scripts/CB.lua"))()
