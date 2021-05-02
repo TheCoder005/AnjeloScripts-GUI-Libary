18eea47c64e2578b3ad675c08be79ad280976910
Anjelo <78515473+TheCoder005@users.noreply.github.com>
2021-05-02 19:48:15 -0400

    Update GameCheck.lua

--- GameCheck.lua
+++ GameCheck.lua
@@ -1,3 +1,4 @@
+print(Theme)
 --Arsunal
 if game.PlaceId == 286090429 then
     local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheCoder005/AnjeloScripts-Hub/TheCoder005-Game-Scripts/Arsenal.lua"))()
