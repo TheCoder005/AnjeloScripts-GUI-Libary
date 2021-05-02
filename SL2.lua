514f610a100bab947f7c9578eb1803fce2cb47e0
Anjelo <78515473+TheCoder005@users.noreply.github.com>
2021-05-01 20:41:42 -0400

    Update SL2.lua

--- SL2.lua
+++ SL2.lua
@@ -1,10 +1,10 @@
 shared.Enabled = true
-shared.Settings = {
-CanGetScroll = true,
-CanRankUp    = false,
-AllowBoss    = true,
-PlayerHeight = -15, -- If u got a free exploit, change this to above -20
-TweenSpeed = 1,
-}
+  shared.Settings = {
+  CanGetScroll = true,
+  CanRankUp    = false,
+  AllowBoss    = true,
+  PlayerHeight = -15, -- If u got a free exploit, change this to above -20
+  TweenSpeed = 1,
+  }
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Godne8825/Avakir/Scripts/Shinobi%20Life%202%20%5BV%3A%201.7%5D", true))()
