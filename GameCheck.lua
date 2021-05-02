1230c2e3beeeb59ce0ebfdf0696c746ea6e63d54
Anjelo <78515473+TheCoder005@users.noreply.github.com>
2021-05-02 19:33:32 -0400

    Update GameCheck.lua

--- GameCheck.lua
+++ GameCheck.lua
@@ -7,4 +7,8 @@ elseif game.PlaceId == 4601350214 then
 --CB    
 elseif game.PlaceId == 301549746 then
      local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheCoder005/AnjeloScripts-Hub/TheCoder005-Game-Scripts/CB.lua"))()
+--Shoot OUt
+elseif game.PlaceId == 738545896 then
+     local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheCoder005/AnjeloScripts-Hub/TheCoder005-Game-Scripts/ShootOut.lua"))()
 end
+
