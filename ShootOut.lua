fe71779852ff3fb3b3ea2c30fdaacca84832f381
Anjelo <78515473+TheCoder005@users.noreply.github.com>
2021-06-09 18:56:50 -0400

    Update ShootOut.lua

--- ShootOut.lua
+++ ShootOut.lua
@@ -1,5 +1,40 @@
-        local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-        local Window = Library.CreateLib("AnjeloScripts ", Theme)
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+    local Window = Library.CreateLib("AnjeloScripts ", Theme)
+    
+    --tabs
+    local Main = Window:NewTab("Main")
+    local AimbotSettings = Main:NewSection("Aimbot Settings")
+    local ESPSetings = Main:NewSection("ESP Settings")
+    local Player = Window:NewTab("Player")
+    local PlayerSection = Player:NewSection("Player")
+    local Settings = Window:NewTab("Settings")
+    local SettingsSection = Settings:NewSection("Settings")
+    --MainSection
+    
+            getgenv().Options = {
+            Enabled = true,
+            TeamCheck = true,
+            Triggerbot = false,
+            Smoothness = true,
+            AimPart = "Head",
+            FOV = 150
+        }
+    
+    AimbotSettings:NewSlider("FOV", "Changes the size of the ring", 500, 1, function(s) -- 500 (MaxValue) | 0 (MinValue)
+            getgenv().Options = {
+                    Enabled = true,
+                    TeamCheck = true,
+                    Triggerbot = false,
+                    Smoothness = true,
+                    AimPart = "Head",
+                    FOV = s
+                } 
+        end)
+        
+    
+    AimbotSettings:NewButton("Aimbot", "Injects Aimbot", function()
+        print("Clicked")
+        
         local Players = game:GetService("Players")
         local RunService = game:GetService("RunService")
         local UserInputService = game:GetService("UserInputService")
@@ -12,108 +47,7 @@
         
         local Down = false
         local Inset = GuiService:GetGuiInset()
-        --tabs
-        local Main = Window:NewTab("Main")
-        local AimbotSettings = Main:NewSection("Aimbot Settings")
-        local ESPSetings = Main:NewSection("ESP Settings")
-        local Player = Window:NewTab("Player")
-        local PlayerSection = Player:NewSection("Player")
-        local Settings = Window:NewTab("Settings")
-        local SettingsSection = Settings:NewSection("Settings")
         
-        --// Aimbot \\--
-        local a = require(game:GetService("ReplicatedFirst")["_0xS0URC3X"].Shared.AimAssistSettings)
-        a.MaxScreenCoverage = 0
-        a.MaxKDR = 0
-        a.MaxRange = 0
-        
-        AimbotSettings:NewToggle("SoftAim", "ToggleInfo", function(state)
-            if state then
-                print("Toggle On")
-                a.MaxScreenCoverage = 999999
-                a.MaxKDR = 99999999.99999999
-                a.MaxRange = 999999999
-            else
-                print("Toggle Off")
-                a.MaxScreenCoverage = 0
-                a.MaxKDR = 0
-                a.MaxRange = 0
-            end
-        end)
-        
-        AimbotSettings:NewSlider("FOV", "Changes the size of the ring", 500, 1, function(s) -- 500 (MaxValue) | 0 (MinValue)
-            getgenv().Options = {
-                    Enabled = true,
-                    TeamCheck = true,
-                    Triggerbot = false,
-                    Smoothness = true,
-                    AimPart = "Head",
-                    FOV = s
-                } 
-        end)
-        --// ESP \\--
-                
-        ESPSetings:NewToggle("ToggleText", "ToggleInfo", function(state)
-            if state then
-                print("Toggle On")
-                local color = BrickColor.new(50,0,250)
-                local transparency = .8
-                
-                local Players = game:GetService("Players")
-                local function _ESP(c)
-                  repeat wait() until c.PrimaryPart ~= nil
-                  for i,p in pairs(c:GetChildren()) do
-                    if p.ClassName == "Part" or p.ClassName == "MeshPart" then
-                      if p:FindFirstChild("shit") then p.shit:Destroy() end
-                      local a = Instance.new("BoxHandleAdornment",p)
-                      a.Name = "shit"
-                      a.Size = p.Size
-                      a.Color = color
-                      a.Transparency = transparency
-                      a.AlwaysOnTop = true    
-                      a.Visible = true    
-                      a.Adornee = p
-                      a.ZIndex = true    
-                
-                    end
-                  end
-                end
-                local function ESP()
-                  for i,v in pairs(Players:GetChildren()) do
-                    if v ~= game.Players.LocalPlayer then
-                      if v.Character then
-                        _ESP(v.Character)
-                      end
-                      v.CharacterAdded:Connect(function(chr)
-                        _ESP(chr)
-                      end)
-                    end
-                  end
-                  Players.PlayerAdded:Connect(function(player)
-                    player.CharacterAdded:Connect(function(chr)
-                      _ESP(chr)
-                    end)  
-                  end)
-                end
-                ESP()
-            else
-                print("Toggle Off")
-            end
-        end)
-        
-        
-        --// Player \\--
-        PlayerSection:NewSlider("WalkSpeed", "changes how fast you walk", 500, 16, function(s) -- 500 (MaxValue) | 0 (MinValue)
-            game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
-        end)
-    
-        PlayerSection:NewSlider("Jump Power", "changes how high you jump", 500, 50, function(s) -- 500 (MaxValue) | 0 (MinValue)
-            game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
-        end)
-        --// Settings \\--
-        SettingsSection:NewKeybind("KeybindText", "KeybindInfo", Enum.KeyCode.F, function()
-    	    Library:ToggleUI()
-        end)
         --// Functions \\--
         local gc = function()
         	local nearest = math.huge
@@ -150,7 +84,7 @@
             circ.Color = Color3.fromRGB(255,255,255)
         	circ.Filled = false
         	circ.NumSides = 150
-            circ.Radius = Options.FOV or 16
+            circ.Radius = Options.FOV
             return circ
         end
         
@@ -195,3 +129,137 @@
                 curc.Visible = true
             end
         end)
+    end)
+        
+    ESPSetings:NewButton("ESP", "Injects ESP", function()
+        print("Clicked")
+        local Holder = Instance.new("Folder", game.CoreGui)
+        Holder.Name = "ESP"
+        
+        local Box = Instance.new("BoxHandleAdornment")
+        Box.Name = "nilBox"
+        Box.Size = Vector3.new(4, 7, 4)
+        Box.Color3 = Color3.new(100 / 255, 100 / 255, 100 / 255)
+        Box.Transparency = 0.7
+        Box.ZIndex = 0
+        Box.AlwaysOnTop = true
+        Box.Visible = true
+        
+        local NameTag = Instance.new("BillboardGui")
+        NameTag.Name = "nilNameTag"
+        NameTag.Enabled = false
+        NameTag.Size = UDim2.new(0, 200, 0, 50)
+        NameTag.AlwaysOnTop = true
+        NameTag.StudsOffset = Vector3.new(0, 1.8, 0)
+        local Tag = Instance.new("TextLabel", NameTag)
+        Tag.Name = "Tag"
+        Tag.BackgroundTransparency = 1
+        Tag.Position = UDim2.new(0, -50, 0, 0)
+        Tag.Size = UDim2.new(0, 300, 0, 20)
+        Tag.TextSize = 20
+        Tag.TextColor3 = Color3.new(100 / 255, 100 / 255, 100 / 255)
+        Tag.TextStrokeColor3 = Color3.new(0 / 255, 0 / 255, 0 / 255)
+        Tag.TextStrokeTransparency = 0.4
+        Tag.Text = "nil"
+        Tag.Font = Enum.Font.SourceSansBold
+        Tag.TextScaled = false
+        
+        local LoadCharacter = function(v)
+        	repeat wait() until v.Character ~= nil
+        	v.Character:WaitForChild("Humanoid")
+        	local vHolder = Holder:FindFirstChild(v.Name)
+        	vHolder:ClearAllChildren()
+        	local b = Box:Clone()
+        	b.Name = v.Name .. "Box"
+        	b.Adornee = v.Character
+        	b.Parent = vHolder
+        	local t = NameTag:Clone()
+        	t.Name = v.Name .. "NameTag"
+        	t.Enabled = true
+        	t.Parent = vHolder
+        	t.Adornee = v.Character:WaitForChild("Head", 5)
+        	if not t.Adornee then
+        		return UnloadCharacter(v)
+        	end
+        	t.Tag.Text = v.Name
+        	b.Color3 = Color3.new(v.TeamColor.r, v.TeamColor.g, v.TeamColor.b)
+        	t.Tag.TextColor3 = Color3.new(v.TeamColor.r, v.TeamColor.g, v.TeamColor.b)
+        	local Update
+        	local UpdateNameTag = function()
+        		if not pcall(function()
+        			v.Character.Humanoid.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.None
+        			local maxh = math.floor(v.Character.Humanoid.MaxHealth)
+        			local h = math.floor(v.Character.Humanoid.Health)
+        			t.Tag.Text = v.Name .. "\n" .. ((maxh ~= 0 and tostring(math.floor((h / maxh) * 100))) or "0") .. "%  " .. tostring(h) .. "/" .. tostring(maxh)
+        		end) then
+        			Update:Disconnect()
+        		end
+        	end
+        	UpdateNameTag()
+        	Update = v.Character.Humanoid.Changed:Connect(UpdateNameTag)
+        end
+        
+        local UnloadCharacter = function(v)
+        	local vHolder = Holder:FindFirstChild(v.Name)
+        	if vHolder and (vHolder:FindFirstChild(v.Name .. "Box") ~= nil or vHolder:FindFirstChild(v.Name .. "NameTag") ~= nil) then
+        		vHolder:ClearAllChildren()
+        	end
+        end
+        
+        local LoadPlayer = function(v)
+        	local vHolder = Instance.new("Folder", Holder)
+        	vHolder.Name = v.Name
+        	v.CharacterAdded:Connect(function()
+        		pcall(LoadCharacter, v)
+        	end)
+        	v.CharacterRemoving:Connect(function()
+        		pcall(UnloadCharacter, v)
+        	end)
+        	v.Changed:Connect(function(prop)
+        		if prop == "TeamColor" then
+        			UnloadCharacter(v)
+        			wait()
+        			LoadCharacter(v)
+        		end
+        	end)
+        	LoadCharacter(v)
+        end
+        
+        local UnloadPlayer = function(v)
+        	UnloadCharacter(v)
+        	local vHolder = Holder:FindFirstChild(v.Name)
+        	if vHolder then
+        		vHolder:Destroy()
+        	end
+        end
+        
+        for i,v in pairs(game:GetService("Players"):GetPlayers()) do
+        	spawn(function() pcall(LoadPlayer, v) end)
+        end
+        
+        game:GetService("Players").PlayerAdded:Connect(function(v)
+        	pcall(LoadPlayer, v)
+        end)
+        
+        game:GetService("Players").PlayerRemoving:Connect(function(v)
+        	pcall(UnloadPlayer, v)
+        end)
+        
+        game:GetService("Players").LocalPlayer.NameDisplayDistance = 0
+    end)
+    
+    --Player
+    
+    PlayerSection:NewSlider("WalkSpeed", "changes how fast you walk", 500, 16, function(s) -- 500 (MaxValue) | 0 (MinValue)
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+    end)
+    
+    PlayerSection:NewSlider("Jump Power", "changes how high you jump", 500, 50, function(s) -- 500 (MaxValue) | 0 (MinValue)
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+    end)
+    
+    -- Settings
+    
+    SettingsSection:NewKeybind("KeybindText", "KeybindInfo", Enum.KeyCode.RightShift, function()
+    	Library:ToggleUI()
+    end)
