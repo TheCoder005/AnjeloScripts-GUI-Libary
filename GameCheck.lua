7bdbe675b8f16a5cb6ca7ebd4ab742b4417a0f92
Anjelo <78515473+TheCoder005@users.noreply.github.com>
2021-09-07 18:27:36 -0400

    Update GameCheck.lua

--- GameCheck.lua
+++ GameCheck.lua
@@ -15,9 +15,7 @@ elseif game.PlaceId == 6744149778 then
 elseif game.PlaceId == 738545896 then
      local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheCoder005/AnjeloScripts-Hub/TheCoder005-Game-Scripts/ShootOut.lua"))()
 --Death Zone
-elseif game.PlaceId == 6010979912 then
-     local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheCoder005/AnjeloScripts-Hub/TheCoder005-Game-Scripts/Death%20Zone.lua"))()
-elseif game.PlaceId == 6010981786 then
+elseif game.PlaceId == 3044959549 then
      local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheCoder005/AnjeloScripts-Hub/TheCoder005-Game-Scripts/Death%20Zone.lua"))()
 --TestWorld
 elseif game.PlaceId == 6571697013 then
